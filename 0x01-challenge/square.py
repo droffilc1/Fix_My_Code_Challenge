f1736ce0435501eea413c7292952d61e3ea33951
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-06 16:16:44 +0300

    fix: my square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -15,7 +15,7 @@ class Square:
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def perimiter_of_my_square(self):
         """ Perimeter of the square """
